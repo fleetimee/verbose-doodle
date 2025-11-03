0b473395b9671734ef265d57e9c6a60134759132
Novian Andika <novian.andika@students.amikom.ac.id>
2025-11-03 09:56:24 +0700

    feat: add fontsource packages and import font styles for Montserrat and Fira Code

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/fira-code/400.css";
+import "@fontsource/fira-code/500.css";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
